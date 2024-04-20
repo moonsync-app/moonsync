e61962cba48b7194ec9906e52d6190997fc087c3
Rui Chen <rui@chenrui.dev>
2024-04-20 18:15:04 -0400

    add some openai integration and test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- api/main.py
+++ api/main.py
@@ -1,12 +1,24 @@
+from http.client import HTTPException
+
 from fastapi import FastAPI
-from starlette.responses import StreamingResponse
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from openai import OpenAI
+from pydantic import BaseModel
+from starlette.responses import StreamingResponse
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
+# Load the .env file
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') or 'OPENAI_API_KEY'
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
 origins = [
-    "http://localhost:3000"
+    "http://localhost:8000"
 ]
 
 app.add_middleware(
@@ -20,3 +32,15 @@ app.add_middleware(
 @app.get("/")
 async def main_route():
   return {"message": "Hey, It is me Rui"}
+
+@app.get("/openai/")
+async def create_completion():
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        ]
+    )
+
+    return {"message": completion.choices[0].message.content}
