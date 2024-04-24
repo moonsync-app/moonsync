cdf04326109e44ab79de930992bec9b0fb84cc07
Sugam Devare <32786748+sugamxp@users.noreply.github.com>
2024-04-24 19:56:58 -0400

    feat - ui improvements

--- app/components/ui/chat/chat-avatar.tsx
+++ app/components/ui/chat/chat-avatar.tsx
@@ -11,7 +11,7 @@ export default function ChatAvatar({ role }: { role: string }) {
   }
 
   return (
-    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow">
+    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-white text-white shadow">
       <Image
         className="rounded-md"
         src="/moon.png"
