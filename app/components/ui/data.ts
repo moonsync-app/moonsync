7f3a2b81596b831771e4ae8409fbd4a2c9943c79
Sugam Devare <32786748+sugamxp@users.noreply.github.com>
2024-04-24 23:06:20 -0400

    feat - add phase visualization

---
+++ app/components/ui/data.ts
@@ -0,0 +1,8 @@
+export const data = [
+  { name: "Flow", value: 5 },
+  { name: "Go", value: 6 },
+  { name: "Sustain", value: 8 },
+  { name: "Reflect", value: 9 },
+];
+
+export const currentDay = 20; // Add the currentDay variable
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,17 @@ import { Poppins } from 'next/font/google';
 import SpeedDial from './components/ui/speed-dial';
 import StatsComponent from './home/stats';
 import Header from './home/header';
+import { DonutChart } from './components/ui/phase';
 
 const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
 export default async function Page() {
   return (
-    <main className={`flex min-h-screen flex-col items-center gap-10 p-4 background-gradient ${poppins.className}`}>
+    <main className={`flex min-h-screen flex-col items-center p-4 background-gradient ${poppins.className}`}>
       <SpeedDial/>
       <Header/>
+      {/* TODO make dynamic */}
+      <DonutChart width={250} height={250} />
       <StatsComponent />
     </main>
   );
