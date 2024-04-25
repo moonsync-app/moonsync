1eb7e257f7c880ddcfa5ea353c1b4c007d8f2f00
Zui Dighe <zuirox@gmail.com>
2024-04-25 18:54:06 -0400

    Update stats-card.tsx, change background color and card positioning

--- app/home/stats-card.tsx
+++ app/home/stats-card.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
   /* <div className="relative h-48 mx-4 -mt-16 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"> */
 }
 
-export default function StatsCard({ svgIllustration, title, details }) {
+export default function StatsCard({ title, details }) {
   return (
-   /* <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-11/12"> */
-    <div className="relative flex flex-col mt-4 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-11/12" style={{ backgroundColor: 'rgba(109, 196, 161, 0.3)' }}>
-      {/* <div className="relative h-48 mx-4 -mt-8 overflow-hidden text-white shadow-sm bg-clip-border rounded-xl"> */}
-      {/*   <Image src={svgIllustration} alt="card-image" className="object-fill" /> */}
-      {/* </div> */}
+    <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-11/12" style={{ backgroundColor: 'rgba(188, 247, 208, 0.5)' }}>
+      {/* <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-11/12"> */}
+      {/*  <div className="relative h-48 mx-4 -mt-8 overflow-hidden text-white shadow-sm bg-clip-border rounded-xl"> */}
+        {/* <Image src={svgIllustration} alt="card-image" className="object-fill" /> */}
+      {/*  </div> */}
       <div className="p-6">
         <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 text-center">
           {title}
