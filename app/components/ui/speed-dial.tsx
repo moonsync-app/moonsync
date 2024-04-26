d4db4c2463bdacbeaa9139d3302b578d84bd0394
Sugam Devare <32786748+sugamxp@users.noreply.github.com>
2024-04-26 14:53:35 -0400

    fix - valid dom property

--- app/components/ui/speed-dial.tsx
+++ app/components/ui/speed-dial.tsx
@@ -29,31 +29,31 @@ export default function SpeedDial() {
           xmlns="http://www.w3.org/2000/svg"
           stroke="#ffffff"
         >
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></g>
           <g id="SVGRepo_iconCarrier">
             {' '}
             <path
               d="M4 7L7 7M20 7L11 7"
               stroke="#ffffff"
-              stroke-width="1.5"
-              stroke-linecap="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
             ></path>{' '}
             <path
               d="M20 17H17M4 17L13 17"
               stroke="#ffffff"
-              stroke-width="1.5"
-              stroke-linecap="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
             ></path>{' '}
             <path
               d="M4 12H7L20 12"
               stroke="#ffffff"
-              stroke-width="1.5"
-              stroke-linecap="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
             ></path>{' '}
           </g>
         </svg>
