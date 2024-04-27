cd764dcb4f9a0daf6fac547455c525f08c5cf3ee
Sugam Devare <32786748+sugamxp@users.noreply.github.com>
2024-04-26 21:03:08 -0400

    feat - improving spacing

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -7,7 +7,7 @@ import SpeedDial from '../components/ui/speed-dial';
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center gap-10 p-12 background-gradient">
+      <main className="flex min-h-screen flex-col items-center gap-10 p-4 pt-20 lg:p-12 background-gradient">
         <SpeedDial/>
         {/* <Header /> */}
         <Suspense>
--- app/components/chat-section.tsx
+++ app/components/chat-section.tsx
@@ -48,7 +48,7 @@ export default function ChatSection() {
   }, [messages, data]);
 
   return (
-    <div className="space-y-4 max-w-6xl w-full">
+    <div className="space-y-4 max-w-8xl sm:max-w-8xl md:max-w-6xl  w-full">
       <ChatMessages
         messages={transformedMessages}
         isLoading={isLoading}
