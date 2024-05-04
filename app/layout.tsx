1ea961e11707ad549067fe3b9c6f6c1a6f7dc781
Rui Chen <rui@chenrui.dev>
2024-05-04 15:02:12 -0400

    update clerk integration setup
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,24 +16,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <ClerkProvider
-        appearance={{
-          variables: { colorPrimary: '#000000' },
-          elements: {
-            formButtonPrimary: 'bg-black border border-black border-solid hover:bg-white hover:text-black',
-            socialButtonsBlockButton:
-              'bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black',
-            socialButtonsBlockButtonText: 'font-semibold',
-            formButtonReset:
-              'bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black',
-            membersPageInviteButton: 'bg-black border border-black border-solid hover:bg-white hover:text-black',
-            card: 'bg-[#fafafa]',
-          },
-        }}
-      >
-        <body className={inter.className}>{children}</body>
-      </ClerkProvider>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <main>
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
