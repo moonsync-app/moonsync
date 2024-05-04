fdff1a0a1ff51eb9a9c9137c3d31ceb8c4f360d3
Rui Chen <rui@chenrui.dev>
2024-05-04 00:32:04 -0400

    feat: add clerk signup/login integration
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_live_Y2xlcmsubW9vbnN5bmMuYXBwJA',
+  },
   webpack: (config) => {
     // See https://webpack.js.org/configuration/resolve/#resolvealias
     config.resolve.alias = {
