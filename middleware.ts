293e5d29c845d8e977091a2f1332f66a57138fa5
Rui Chen <rui@chenrui.dev>
2024-05-04 00:32:04 -0400

    auth: add clerk login integration
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"
+  ],
+};
