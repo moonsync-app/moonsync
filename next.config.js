9aea15aeb9b3fa1ce8be0b2c4da9b3148d39ba30
Rui Chen <rui@chenrui.dev>
2024-06-04 16:47:25 -0400

    choree: update next config to include public configs
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: "https://accounts.moonsync.app/sign-up",
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: "https://accounts.moonsync.app/sign-in",
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NODE_ENV === "production"
         ? "pk_live_Y2xlcmsubW9vbnN5bmMuYXBwJA"
         : "pk_test_YWxsb3dpbmctam9leS01My5jbGVyay5hY2NvdW50cy5kZXYk",
+    NEXT_PUBLIC_BACKEND_HOST: "https://app.moonsync.app",
   },
   webpack: (config) => {
     // See https://webpack.js.org/configuration/resolve/#resolvealias
