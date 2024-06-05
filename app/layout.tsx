f5eaa316edb321f04a10b21c6625c8214c0ac16f
Rui Chen <rui@chenrui.dev>
2024-06-05 10:05:37 -0400

    enable nextjs analytics
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -46,6 +47,7 @@ export default function RootLayout({
               ></div>
             </div>
             {children}
+            <Analytics />
           </main>
         </body>
       </html>
