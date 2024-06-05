79d61e2580c8171c7d37aeafd0a70b83a5916a88
Rui Chen <rui@chenrui.dev>
2024-06-05 09:53:53 -0400

    chore: remove banner notice
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,28 +48,6 @@ export default function RootLayout({
                   }}
                 ></div>
               </div>
-              <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-                <p className="text-sm leading-6 text-gray-900">
-                  <strong className="font-bold">Note</strong>
-                  <svg
-                    viewBox="0 0 2 2"
-                    className="mx-2 inline h-0.5 w-0.5 fill-current"
-                    aria-hidden="true"
-                  >
-                    <circle cx="1" cy="1" r="1" />
-                  </svg>
-                  The biometric information provided is from a sample user. You
-                  will have the option to link your own data shortly.
-                </p>
-              </div>
-              <div className="flex flex-1 justify-end">
-                <button
-                  type="button"
-                  className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
-                >
-                  <span className="sr-only">Dismiss</span>
-                </button>
-              </div>
             </div>
             {children}
           </main>
