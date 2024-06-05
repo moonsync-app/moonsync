44c83536ead8da3a13ff44236e4608e5fb26f628
Rui Chen <rui@chenrui.dev>
2024-06-05 14:48:55 -0400

    chore(config): add cors setup for vercel analytics
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- next.config.js
+++ next.config.js
@@ -23,6 +23,33 @@ const nextConfig = {
     },
   },
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Content-Type, api_key, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
