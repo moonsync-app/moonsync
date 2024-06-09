8fc1433badb6f7cacc062472f189929e089c8085
Rui Chen <rui@chenrui.dev>
2024-06-08 23:31:01 -0400

    update migration files
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ app/drizzle/migrations/0001_smart_moira_mactaggert.sql
@@ -0,0 +1,4 @@
+DROP TABLE "moonsync"."chat";--> statement-breakpoint
+DROP TABLE "moonsync"."onboarding";--> statement-breakpoint
+DROP TABLE "moonsync"."user";--> statement-breakpoint
+DROP SCHEMA "moonsync";
