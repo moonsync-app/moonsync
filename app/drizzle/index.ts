821fd7fb5aefa3cc12ffcf9e738e4de44dfb79cf
Rui Chen <rui@chenrui.dev>
2024-06-09 00:04:04 -0400

    polish the db client
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- app/drizzle/index.ts
+++ app/drizzle/index.ts
@@ -1,17 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-import * as schema from "./schema";
+const connectionString = process.env.DATABASE_URL as string;
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
+const client = postgres(connectionString);
 
-const conn = globalForDb.conn ?? postgres(process.env.DATABASE_URL as string);
-if (process.env.NODE_ENV !== "production") globalForDb.conn = conn;
-
-export const db = drizzle(conn, { schema });
+export const db = drizzle(client);
