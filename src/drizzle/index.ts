f7d5084dd786c88af6deb93535c7f3ccdb68d5a5
Rui Chen <rui@chenrui.dev>
2024-06-10 14:46:16 -0400

    update index.ts
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- src/drizzle/index.ts
+++ src/drizzle/index.ts
@@ -2,8 +2,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL as string;
+const conn = postgres(process.env.DATABASE_URL!);
 
-const client = postgres(connectionString);
-
-export const db = drizzle(client, { schema });
+export const db = drizzle(conn, { schema });
