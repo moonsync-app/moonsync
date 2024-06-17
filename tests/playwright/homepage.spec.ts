e19900887ab7f73fbc3f57b4ed2101542222cd91
Rui Chen <rui@chenrui.dev>
2024-06-17 18:32:35 -0400

    update e2e test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- tests/playwright/homepage.spec.ts
+++ tests/playwright/homepage.spec.ts
@@ -6,22 +6,21 @@ test("sign-in page has title and necessary elements", async ({ page }) => {
 
   await page.waitForSelector(".cl-header");
 
-  // // Check that the page has the correct title
-  // await expect(page).toHaveTitle("Sign in to Moonsync");
+  // Check that the page has the correct title
+  await expect(page).toHaveTitle("My account | Moonsync");
 
   // Check for the "Continue with Google" button
   const googleSignInButton = page.locator("text=Continue with Google");
   await expect(googleSignInButton).toBeVisible();
 
-  // Check for the email input field
-  const emailInput = page.locator('input[type="email"]');
-  await expect(emailInput).toBeVisible();
-
   // Check for the "Continue" button
-  const continueButton = page.locator("text=Continue");
+  const continueButton = page.getByRole("button", {
+    name: "Continue",
+    exact: true,
+  });
   await expect(continueButton).toBeVisible();
 
   // Optionally, check for the "Sign up" link
-  const signUpLink = page.locator("text=Sign up");
+  const signUpLink = page.getByRole("link", { name: "Sign up", exact: true });
   await expect(signUpLink).toBeVisible();
 });
